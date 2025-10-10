bc7ff381cafa04627dabd78158fafbb9e2ff5b66
manglemix <najmanhusaini20@gmail.com>
2025-10-09 22:14:59 -0600

    remove general training

--- general-training/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![recursion_limit = "256"]
-
-#[cfg(feature = "app")]
-pub mod app;
-// pub mod batches;
-// pub mod dataset;
-pub mod regression;
-pub mod training_loop;
--- general-training/src/main.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "app")]
-use general_training::app::main;
-
-#[cfg(not(feature = "app"))]
-fn main() {
-    println!("The `app` feature needs to be enabled")
-}
